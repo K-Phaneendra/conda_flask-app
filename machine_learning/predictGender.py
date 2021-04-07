e84223c552be09e45da60472d85a7bcecff79ce2
K-Phaneendra <phaneendra.kosanam@hotmail.com>
2021-04-08 01:03:02 +0530

    added opencv to aptfile

--- machine_learning/predictGender.py
+++ machine_learning/predictGender.py
@@ -1,7 +1,6 @@
 import os
 import numpy as np
 import pandas as pd
-# import matplotlib.pyplot as plt
 import cv2
 import pickle # save the structured data frame in pickle format. output will be a .pickle file
 import sklearn
